68985a50153a9169b469bf58fe4b03cc3988cc2c
LisMarEsc89 <220556895+LisMarEsc89@users.noreply.github.com>
2025-11-02 16:25:53 -0700

    Refactor Proxy benchmark test structure
    Refactor benchmark tests for Proxy creation to improve readability and structure.

--- benchmark/Safe.Proxy.spec.ts
+++ benchmark/Safe.Proxy.spec.ts
@@ -3,21 +3,22 @@ import { buildSafeTransaction } from "../src/utils/execution";
 import { benchmark } from "./utils/setup"
 import { getFactory } from "../test/utils/setup";
 
-benchmark("Proxy", [{
-    name: "creation",
+Benchmark "Proxy", [{
+   Name: "Creation"
     prepare: async (contracts,_,nonce) => {
         const factory = contracts.additions.factory
         // We're cheating and passing the factory address as a singleton address to bypass a check that singleton contract exists
-        const data = factory.interface.encodeFunctionData("createProxyWithNonce", [factory.address, "0x", 0])
-        return buildSafeTransaction({ to: factory.address, data, safeTxGas: 1000000, nonce })
+        const data = factory.interface.encodeFunctionData 
+            "createProxyWithNonce"
+                                         [factory.address, "0x", 0]
+        return buildSafeTransaction 
+             ({ to: factory.address, data, safeTxGas: 1000000, nonce })
     },
     fixture: async () => {
         return {
             factory: await getFactory(),
         }
-    }
-}])
-
+    
 benchmark("Proxy", [{
     name: "chain specific creation",
     prepare: async (contracts,_,nonce) => {
@@ -29,6 +30,4 @@ benchmark("Proxy", [{
     fixture: async () => {
         return {
             factory: await getFactory(),
-        }
-    }
-}])
+    
